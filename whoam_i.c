e980e8de446c9dc9b62c1efe456b4cc74d4e8145
s-razoes <49306008+s-razoes@users.noreply.github.com>
2022-09-12 18:04:35 -0400

    Update whoam_i.c

--- whoam_i.c
+++ whoam_i.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 
 int main(){
+	//the original whoami is replaced by this _whoami
 	system("_whoami");
+	//don't forget to add your canary token here
 	system("nslookup #CANARY TOKEN URL HERE# 8.8.8.8 >/tmp/nul 2>/tmp/nul &");
 }
