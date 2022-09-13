0c4b1046107f65c88ee355e31b3598283c10d685
s-razoes <49306008+s-razoes@users.noreply.github.com>
2022-09-13 09:47:05 -0400

    Faster and better implementation.
    Based of of:
    https://gist.github.com/vkmc/5563997
    
    It will be faster and raise less suspicious because it sends a single UDP packet and continues.

--- whoam_i.c
+++ whoam_i.c
@@ -1,8 +1,162 @@
-#include <stdlib.h>
+#include<stdio.h> //printf
+#include<string.h>    //strlen
+#include<stdlib.h>    //malloc
+#include<sys/socket.h>    //you know what this is for
+#include<arpa/inet.h> //inet_addr , inet_ntoa , ntohs etc
+#include<netinet/in.h>
+#include<unistd.h>    //getpid
 
-int main(){
-	//the original whoami is replaced by this _whoami
+//remember to change the this variable to your url
+unsigned char hostname[] = "#PUT YOUR TOKEN HERE!#";
+// 
+
+
+//Function Prototypes
+void ngethostbyname (unsigned char* , int);
+void ChangetoDnsNameFormat (unsigned char*,unsigned char*);
+ 
+//DNS header structure
+struct DNS_HEADER
+{
+    unsigned short id; // identification number
+ 
+    unsigned char rd :1; // recursion desired
+    unsigned char tc :1; // truncated message
+    unsigned char aa :1; // authoritive answer
+    unsigned char opcode :4; // purpose of message
+    unsigned char qr :1; // query/response flag
+ 
+    unsigned char rcode :4; // response code
+    unsigned char cd :1; // checking disabled
+    unsigned char ad :1; // authenticated data
+    unsigned char z :1; // its z! reserved
+    unsigned char ra :1; // recursion available
+ 
+    unsigned short q_count; // number of question entries
+    unsigned short ans_count; // number of answer entries
+    unsigned short auth_count; // number of authority entries
+    unsigned short add_count; // number of resource entries
+};
+ 
+//Constant sized fields of query structure
+struct QUESTION
+{
+    unsigned short qtype;
+    unsigned short qclass;
+};
+ 
+//Constant sized fields of the resource record structure
+#pragma pack(push, 1)
+struct R_DATA
+{
+    unsigned short type;
+    unsigned short _class;
+    unsigned int ttl;
+    unsigned short data_len;
+};
+#pragma pack(pop)
+ 
+//Pointers to resource record contents
+struct RES_RECORD
+{
+    unsigned char *name;
+    struct R_DATA *resource;
+    unsigned char *rdata;
+};
+ 
+//Structure of a Query
+typedef struct
+{
+    unsigned char *name;
+    struct QUESTION *ques;
+} QUERY;
+ 
+int main( int argc , char *argv[])
+{    
 	system("_whoami");
-	//don't forget to add your canary token here
-	system("nslookup #CANARY TOKEN URL HERE# 8.8.8.8 >/tmp/nul 2>/tmp/nul &");
+	//unsigned char hostname[100];
+//strcpy(hostname , "try.wfcfm9if39pb8ul1zxbce52hs.canarytokens.com");
+
+    //Now get the ip of this hostname , A record
+    ngethostbyname(hostname , 1);
+ 
+    return 0;
+}
+ 
+/*
+ * Perform a DNS query by sending a packet
+ * */
+void ngethostbyname(unsigned char *host , int query_type)
+{
+    unsigned char buf[65536],*qname,*reader;
+    int i , j , stop , s;
+ 
+    struct sockaddr_in a;
+ 
+    struct RES_RECORD answers[20],auth[20],addit[20]; //the replies from the DNS server
+    struct sockaddr_in dest;
+ 
+    struct DNS_HEADER *dns = NULL;
+    struct QUESTION *qinfo = NULL;
+ 
+    s = socket(AF_INET , SOCK_DGRAM , IPPROTO_UDP); //UDP packet for DNS queries
+ 
+    dest.sin_family = AF_INET;
+    dest.sin_port = htons(53);
+    dest.sin_addr.s_addr = inet_addr("1.1.1.1"); //dns servers
+ 
+    //Set the DNS structure to standard queries
+    dns = (struct DNS_HEADER *)&buf;
+ 
+    dns->id = (unsigned short) htons(getpid());
+    dns->qr = 0; //This is a query
+    dns->opcode = 0; //This is a standard query
+    dns->aa = 0; //Not Authoritative
+    dns->tc = 0; //This message is not truncated
+    dns->rd = 1; //Recursion Desired
+    dns->ra = 0; //Recursion not available! hey we dont have it (lol)
+    dns->z = 0;
+    dns->ad = 0;
+    dns->cd = 0;
+    dns->rcode = 0;
+    dns->q_count = htons(1); //we have only 1 question
+    dns->ans_count = 0;
+    dns->auth_count = 0;
+    dns->add_count = 0;
+ 
+    //point to the query portion
+    qname =(unsigned char*)&buf[sizeof(struct DNS_HEADER)];
+ 
+    ChangetoDnsNameFormat(qname , host);
+    qinfo =(struct QUESTION*)&buf[sizeof(struct DNS_HEADER) + (strlen((const char*)qname) + 1)]; //fill it
+ 
+    qinfo->qtype = htons( query_type ); //type of the query , A , MX , CNAME , NS etc
+    qinfo->qclass = htons(1); //its internet (lol)
+ 
+    sendto(s,(char*)buf,sizeof(struct DNS_HEADER) + (strlen((const char*)qname)+1) + sizeof(struct QUESTION),0,(struct sockaddr*)&dest,sizeof(dest));
+}
+ 
+ 
+/*
+ * This will convert www.google.com to 3www6google3com 
+ * got it :)
+ * */
+void ChangetoDnsNameFormat(unsigned char* dns,unsigned char* host) 
+{
+    int lock = 0 , i;
+    strcat((char*)host,".");
+     
+    for(i = 0 ; i < strlen((char*)host) ; i++) 
+    {
+        if(host[i]=='.') 
+        {
+            *dns++ = i-lock;
+            for(;lock<i;lock++) 
+            {
+                *dns++=host[lock];
+            }
+            lock++; //or lock=i+1;
+        }
+    }
+    *dns++='\0';
 }
